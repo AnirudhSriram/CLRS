d39a21ec9202dea0de2047abfe24d1cf06dd81cb
Anirudh Sriram <anirudhsriram1995@gmail.com>
2018-08-28 20:32:44 +0530

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Anirudh Sriram on 8/28/18.
+//  Copyright © 2018 Anirudh Sriram. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

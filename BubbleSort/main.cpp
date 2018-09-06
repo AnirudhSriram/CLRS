9b860a266a479dc6a82e0d8a6b7526217b98a773
Anirudh Sriram <anirudhsriram1995@gmail.com>
2018-09-06 20:13:32 +0530

    Anirudh : Bubble Sort implementation

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -7,9 +7,28 @@
 //
 
 #include <iostream>
-
+using namespace std ;
+void bubbleSort(int arr[],int n) {
+    int j = n ;
+    while(j>=0) {
+        for(int i=0;i<n-1;i++) {
+            if(arr[i]>arr[i+1]) {
+                int temp = arr[i] ;
+                arr[i] = arr[i+1] ;
+                arr[i+1] = temp ;
+            }
+        }
+        j-- ; 
+    }
+    
+    
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n = 5 ;
+    int a[] = {7,10,22,4,0} ;
+    bubbleSort(a,n) ;
+    for(int i=0;i<n;i++){
+        cout << a[i];
+    }
     return 0;
 }
